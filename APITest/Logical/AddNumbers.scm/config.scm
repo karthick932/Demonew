6218b95cd2d8d4fd2d993950987cb37b7b437c18
Karthick Balu <karthick_ba@prod.hclpnp.com>
2024-02-26 11:27:10 +0530

    GitCommitFeb

---
+++ APITest/Logical/AddNumbers.scm/config.scm
@@ -0,0 +1,7 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<editableResource createdTimestamp="1708687525820"
+    createdUser="karthick_ba" id="2f67b12b:18dd5b58e00:-7ec2"
+    type="service_component_resource" updatedTimestamp="1708687525820"
+    updatedUser="karthick_ba" version="1.1">
+    <loggingConfiguration debug="false" resultWriterLoggingLevel="FULL" writeStubEventsToDB="false"/>
+</editableResource>
---
+++ PerfTest/PerformanceDemoProject/src/test/VUSchedule_Schedule_A1EED23D4B904C50E1FAB66432316537.java
@@ -0,0 +1,105 @@
+/**@generated
+ * WARNING � Changes you make to this file may be lost.
+ *           File is generated and may be re-generated without warning.
+ * @RPT-Core-generated Version 8.6
+ */
+package test;
+
+import com.ibm.rational.test.lt.execution.http.tes.*;
+import com.ibm.rational.test.lt.kernel.action.IContainer;
+import com.ibm.rational.test.lt.kernel.action.IKThinkControl;
+import com.ibm.rational.test.lt.kernel.action.impl.Container;
+import com.ibm.rational.test.lt.kernel.action.impl.KThrow;
+import com.ibm.rational.test.lt.kernel.action.impl.UserGroup;
+import com.ibm.rational.test.lt.kernel.data.FeatureOptionsDataAreaCreation;
+import com.ibm.rational.test.lt.kernel.services.*;
+import com.ibm.rational.test.lt.kernel.services.RPTEventStructure;
+import java.util.HashMap;
+
+@SuppressWarnings("all")
+public class VUSchedule_Schedule_A1EED23D4B904C50E1FAB66432316537
+		extends com.ibm.rational.test.lt.kernel.action.impl.Schedule {
+
+	public VUSchedule_Schedule_A1EED23D4B904C50E1FAB66432316537(IContainer parent, String name) {
+		super(parent, name, "A1EED23D4B904C50E1FAB66432316537");
+		super.actualScheduleName = "VUSchedule";
+		setThinkMax(10000);
+		setThinkScheme(IKThinkControl.RECORDED);
+
+	}
+
+	public void execute() {
+
+		this.addEventBehavior(
+				new RPTEventStructure(new HTTPPageTitleVPEvent(), null, 1, "Page title verification failed"));
+		this.addEventBehavior(
+				new RPTEventStructure(new HTTPResponseCodeVPEvent(), null, 1, "Response code verification failed"));
+		this.addEventBehavior(
+				new RPTEventStructure(new HTTPResponseSizeVPEvent(), null, 1, "Response size verification failed"));
+		this.addEventBehavior(
+				new RPTEventStructure(new RPTFailVPEvent(), null, 1, "Content Verification Point failed"));
+		this.addEventBehavior(new RPTEventStructure(new RPTConnectEvent(), null, 1, "Connection failed"));
+		this.addEventBehavior(new RPTEventStructure(new RPTAuthenticationEvent(), null, 1, "Authentication failed"));
+		this.addEventBehavior(new RPTEventStructure(new RPTDataPoolEOFEvent(),
+				new RPTStopUserEvent("End of dataset reached"), 1, "End of dataset reached"));
+		this.addEventBehavior(
+				new RPTEventStructure(new RPTTestVerdictFailedEvent(), null, 2, "Test verdict was set to fail"));
+		this.addEventBehavior(new RPTEventStructure(new RPTReferenceEvent(), null, 1, "Failed to extract reference"));
+		this.addEventBehavior(new RPTEventStructure(new RPTSubstitutionEvent(), null, 1, "Substitution failed"));
+		this.addEventBehavior(new RPTEventStructure(new RPTServerTimeoutEvent(), null, 1, "Timeout"));
+		this.addEventBehavior(
+				new RPTEventStructure(new RPTDroppedConnectionEvent(), null, 1, "Server dropped connection"));
+		this.addEventBehavior(
+				new RPTEventStructure(new RPTCustomCodeVPFailureEvent(), null, 1, "Custom Verification Point failed"));
+		this.addEventBehavior(new RPTEventStructure(new RPTCustomCodeExceptionEvent(),
+				new RPTStopUserEvent("Custom Code reported an unhandled exception"), 1,
+				"Custom Code reported an unhandled exception"));
+
+		this.addUserGroup(new UserGroup_1(this));
+
+		setMaxHealthFailuresToLog(50, -1, 0);
+		super.execute();
+	}
+
+	public class UserGroup_1 extends UserGroup {
+
+		public UserGroup_1(IContainer parent) {
+			super(parent, "User Group 1", "A1EED23D4B9C5A44E1FAB66432316537");
+
+			setNumUsers(100.0);
+
+		}
+
+		public IContainer createTesterWorkload(IContainer parent) {
+			return scenario_1(parent);
+		}
+
+		private Container scenario_1(IContainer parent) {
+			Container scenario = new Container(parent, "Default Scenario", "A1EED23D4B9C5A4FE1FAB66432316537") {
+
+				public void reportStopMessage() {
+				}
+
+				public void reportForcedStopMessage() {
+				}
+
+				public void execute() {
+					this.add(new test.Perftest_Test_A1EED23CE9C226B0E1FAB66432316537(this,
+							"A1EED23D5156B8E0E1FAB66432316537") {
+						public void execute() {
+							this.setRtbEnabled(false);
+							super.execute();
+						}
+					});
+
+					this.add(new com.ibm.rational.test.lt.execution.protocol.impl.HTTPUserComplete(this));
+					super.execute();
+				}
+			};
+
+			return scenario;
+		}
+
+	}
+
+}
